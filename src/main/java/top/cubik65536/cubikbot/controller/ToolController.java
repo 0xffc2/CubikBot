1d2472b04a0805d729adab3f907bcf24e4068841
Cubik65536 <cubik65536@outlook.com>
2021-01-24 15:50:28 -0500

    删除不需要的工具控制器功能

--- src/main/java/top/cubik65536/cubikbot/controller/ToolController.java
+++ src/main/java/top/cubik65536/cubikbot/controller/ToolController.java
@@ -14,8 +14,6 @@ import com.icecreamqaq.yuq.controller.ContextSession;
 import com.icecreamqaq.yuq.entity.Group;
 import com.icecreamqaq.yuq.entity.Member;
 import com.icecreamqaq.yuq.job.RainInfo;
-import com.icecreamqaq.yuq.message.Image;
-import com.icecreamqaq.yuq.message.Message;
 import com.icecreamqaq.yuq.message.MessageItemFactory;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
@@ -23,24 +21,17 @@ import oshi.hardware.GlobalMemory;
 import top.cubik65536.cubikbot.entity.GroupEntity;
 import top.cubik65536.cubikbot.logic.QQAILogic;
 import top.cubik65536.cubikbot.logic.ToolLogic;
-import top.cubik65536.cubikbot.pojo.Result;
 import top.cubik65536.cubikbot.service.ConfigService;
 import top.cubik65536.cubikbot.service.GroupService;
 import top.cubik65536.cubikbot.service.MessageService;
 import top.cubik65536.cubikbot.utils.BotUtils;
-import top.cubik65536.cubikbot.utils.OkHttpUtils;
 
 import javax.inject.Inject;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -75,280 +66,6 @@ public class ToolController {
 
     public ToolController() {
         startTime = LocalDateTime.now();
-    }
-
-    @QMsg(at = true)
-    @Action("百度 {content}")
-    public String teachYouBaidu(String content) throws IOException {
-        return toolLogic.teachYou(content, "baidu");
-    }
-
-    @QMsg(at = true)
-    @Action("谷歌 {content}")
-    public String teachYouGoogle(String content) throws IOException {
-        return toolLogic.teachYou(content, "google");
-    }
-
-    @QMsg(at = true)
-    @Action("bing {content}")
-    public String teachYouBing(String content) throws IOException {
-        return toolLogic.teachYou(content, "bing");
-    }
-
-    @QMsg(at = true)
-    @Action("搜狗 {content}")
-    public String teachYouSouGou(String content) throws IOException {
-        return toolLogic.teachYou(content, "sougou");
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("舔狗日记")
-    public String dogLicking() throws IOException {
-        return toolLogic.dogLicking();
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("百科 {params}")
-    public String baiKe(String params) throws IOException {
-        return toolLogic.baiKe(params);
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("嘴臭")
-    @Synonym({"祖安语录"})
-    public String mouthOdor() throws IOException {
-        return toolLogic.mouthOdor();
-    }
-
-    @QMsg(at = true)
-    @Action("毒鸡汤")
-    public String poisonousChickenSoup() throws IOException {
-        return toolLogic.poisonousChickenSoup();
-    }
-
-    @QMsg(at = true)
-    @Action("名言")
-    public String saying() throws IOException {
-        return toolLogic.saying();
-    }
-
-    @QMsg(at = true)
-    @Action("一言")
-    public String hiToKoTo() throws IOException {
-        return toolLogic.hiToKoTo().get("text");
-    }
-
-    @Action("缩短/{params}")
-    @QMsg(at = true)
-    public String shortUrl(String params){
-        return BotUtils.shortUrl(params);
-    }
-
-    @Action("ip/{params}")
-    @QMsg(at = true)
-    public String queryIp(String params) throws IOException {
-        return toolLogic.queryIp(params);
-    }
-
-    @Action("whois/{params}")
-    @QMsg(at = true, atNewLine = true)
-    public String queryWhois(String params) throws IOException {
-        return toolLogic.queryWhois(params);
-    }
-
-    @Action("icp/{params}")
-    @QMsg(at = true, atNewLine = true)
-    public String queryIcp(String params) throws IOException {
-        return toolLogic.queryIcp(params);
-    }
-
-    @Action("知乎日报")
-    @QMsg(at = true, atNewLine = true)
-    public String zhiHuDaily() throws IOException {
-        return toolLogic.zhiHuDaily();
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("测吉凶")
-    public String qqGodLock(long qq) throws IOException {
-        return toolLogic.qqGodLock(qq);
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("拼音/{params}")
-    public String convertPinYin(String params) throws IOException {
-        return toolLogic.convertPinYin(params);
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("笑话")
-    public String jokes() throws IOException {
-        return toolLogic.jokes();
-    }
-
-    @QMsg(at = true, atNewLine = true)
-    @Action("垃圾/{params}")
-    public String rubbish(String params) throws IOException {
-        return toolLogic.rubbish(params);
-    }
-
-    @Action("解析/{url}")
-    @QMsg(at = true, atNewLine = true)
-    public String parseVideo(String url) throws IOException {
-        return toolLogic.parseVideo(url);
-    }
-
-    @Action("还原/{url}")
-    @QMsg(at = true)
-    public String restoreShortUrl(String url) throws IOException {
-        return toolLogic.restoreShortUrl(url);
-    }
-
-    @Action("ping/{domain}")
-    @QMsg(at = true, atNewLine = true)
-    public String ping(String domain) throws IOException {
-        return toolLogic.ping(domain);
-    }
-
-    @Action("搜 {question}")
-    @QMsg(at = true)
-    public String search(String question) throws IOException {
-        return toolLogic.searchQuestion(question);
-    }
-
-    @Action("qr/{content}")
-    @QMsg(at = true, atNewLine = true)
-    public Message creatQrCode(String content) throws IOException {
-        byte[] bytes = toolLogic.creatQr(content);
-        return FunKt.getMif().imageByInputStream(new ByteArrayInputStream(bytes)).toMessage();
-    }
-
-    @QMsg(at = true)
-    @Action("蓝奏 {url}")
-    public String lanZou(String url, @PathVar(2) String pwd) throws UnsupportedEncodingException {
-        String resultUrl;
-        if (pwd == null)
-            resultUrl = "https://v1.alapi.cn/api/lanzou?url=" + URLEncoder.encode(url, "utf-8");
-        else resultUrl = "https://v1.alapi.cn/api/lanzou?url=" + URLEncoder.encode(url, "utf-8") + "&pwd=$pwd";
-        return BotUtils.shortUrl(resultUrl);
-    }
-
-    @Action("lol周免")
-    @QMsg(at = true, atNewLine = true)
-    public String lolFree() throws IOException {
-        return toolLogic.lolFree();
-    }
-
-    @Action("缩写/{content}")
-    @QMsg(at = true, atNewLine = true)
-    public String abbreviation(String content) throws IOException {
-        return toolLogic.abbreviation(content);
-    }
-
-    @Action("几点了")
-    public Image time() throws IOException {
-        return FunKt.getMif().imageByInputStream(new ByteArrayInputStream(toolLogic.queryTime()));
-    }
-
-    @Action("\\^BV.*\\")
-    @Synonym({"\\^bv.*\\"})
-    @QMsg(at = true)
-    public Message bvToAv(Message message) throws IOException {
-        String bv = message.getBody().get(0).toPath();
-        Result<Map<String, String>> result = toolLogic.bvToAv(bv);
-        if (result.getCode() == 200){
-            Map<String, String> map = result.getData();
-            MessageItemFactory mif = FunKt.getMif();
-            return mif.imageByUrl(map.get("pic")).plus(
-                    "标题：" + map.get("title") + "\n" +
-                            "描述：" + map.get("desc") +
-                            "链接：" + map.get("url")
-            );
-        }else return Message.Companion.toMessage(result.getMessage());
-    }
-
-    @Action("知乎热榜")
-    @QMsg(at = true, atNewLine = true)
-    public String zhiHuHot() throws IOException {
-        List<Map<String, String>> list = toolLogic.zhiHuHot();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < list.size(); i++){
-            Map<String, String> map = list.get(i);
-            sb.append(i + 1).append("、").append(map.get("title")).append("\n");
-        }
-        return sb.deleteCharAt(sb.length() - 1).toString();
-    }
-
-    @Action("分词")
-    @QMsg(at = true, atNewLine = true)
-    public String wordSegmentation(long qq, ContextSession session, Group group) throws IOException {
-        group.sendMessage(FunKt.getMif().at(qq).plus("请输入需要中文分词的内容！！"));
-        Message nextMessage = session.waitNextMessage();
-        return toolLogic.wordSegmentation(Message.Companion.firstString(nextMessage));
-    }
-
-    @Action("acg")
-    public Image acgPic() throws IOException {
-        return FunKt.getMif().imageByUrl(toolLogic.acgPic());
-    }
-
-    @Action("搜图 {img}")
-    @QMsg(at = true)
-    public Message searchImage(Image img) throws IOException {
-        String url = toolLogic.identifyPic(img.getUrl());
-        if (url != null) return FunKt.getMif().imageByUrl(img.getUrl()).plus(url);
-        else return Message.Companion.toMessage("没有找到这张图片！！！");
-    }
-
-    @Action("OCR {img}")
-    @Synonym({"ocr {img}"})
-    @QMsg(at = true, atNewLine = true)
-    public String ocr(Image img) throws IOException {
-        return qqAiLogic.generalOCR(img.getUrl());
-    }
-
-    @Action("github加速 {url}")
-    @QMsg(at = true)
-    public String githubQuicken(ContextSession session, long qq, String url){
-        return BotUtils.shortUrl(toolLogic.githubQuicken(url));
-    }
-
-    @Action("traceroute {domain}")
-    @Synonym({"路由追踪 {domain}"})
-    public String traceRoute(String domain) throws IOException {
-        return toolLogic.traceRoute(domain);
-    }
-
-    @Action("查发言数")
-    public String queryMessage(Group group){
-        Map<Long, Long> map = messageService.findCountQQByGroupAndToday(group.getId());
-        StringBuilder sb = new StringBuilder().append("本群今日发言数统计如下：").append("\n");
-        for (Map.Entry<Long, Long> entry: map.entrySet()){
-            sb.append("@").append(group.get(entry.getKey()).nameCardOrName())
-                    .append("（").append(entry.getKey()).append("）").append("：")
-                    .append(entry.getValue()).append("条").append("\n");
-        }
-        return sb.deleteCharAt(sb.length() - 1).toString();
-    }
-
-    @Action("语音合成 {text}")
-    public Message voice(String text, Group group, long qq) throws IOException {
-        Result<byte[]> result = qqAiLogic.voiceSynthesis(text);
-        if (result.getCode() == 200){
-            return FunKt.getMif().voiceByByteArray(result.getData()).toMessage();
-        }else return FunKt.getMif().at(qq).plus(result.getMessage());
-    }
-
-    @QMsg(at = true)
-    @Action("防红 {url}")
-    public String preventRed(String url) throws IOException {
-        return toolLogic.preventQQRed(url);
-    }
-
-    @Action("点歌 {name}")
-    public Object musicFromQQ(String name) throws IOException {
-        String xmlStr = toolLogic.songByQQ(name);
-        return mif.xmlEx(2, xmlStr);
     }
 
     @Action("统计")
@@ -439,28 +156,6 @@ public class ToolController {
         }
         double tbNumber = gbNumber/FORMAT;
         return new DecimalFormat("#.##TB").format(tbNumber);
-    }
-
-    @Action("genshin {id}")
-    public String queryGenShinUserInfo(long id) throws IOException {
-        return toolLogic.genShinUserInfo(id);
-    }
-
-    @Action("kuku上传 {image}")
-    @QMsg(at = true)
-    public String uploadImage(Image image){
-        try {
-            return "您上传的图片链接如下：" + toolLogic.uploadImage(OkHttpUtils.getBytes(image.getUrl()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "图片上传失败，请稍后再试！！";
-        }
-    }
-
-    @Action("抽象话 {word}")
-    @QMsg(at = true)
-    public String abstractWords(String word) {
-        return "抽象话如下：\n" + toolLogic.abstractWords(word);
     }
 
     @Action("学习 {learn} {say}")
